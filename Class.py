15dc7ca1c153ab5cac6256768ce76a1339fcf01a
Diana-nyamai <dnyamai.dn@gmail.com>
2021-05-02 18:57:29 +0300

    update

---
+++ Class.py
@@ -0,0 +1,11 @@
+class table:
+    def __init__(self, name, last):
+        self.name = name
+        self.last = last
+    
+    def bark(self):
+        print('woof')
+
+final = table("diana","ndinda")
+print(final.last)
+final.bark()
