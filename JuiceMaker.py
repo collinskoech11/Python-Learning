f605419e9a76ef8933002bb1e69127e0133ffbde
Diana-nyamai <dnyamai.dn@gmail.com>
2021-05-04 16:29:10 +0300

    Juice maker problem

---
+++ JuiceMaker.py
@@ -0,0 +1,15 @@
+class Juice:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+
+    def __str__(self):
+        return (self.name + ' ('+str(self.capacity)+'L)')
+
+    def __add__(self, other):
+        return Juice(self.name + ' & ' + other.name, self.capacity + other.capacity)
+
+x = Juice('orange',6)
+y = Juice('mango', 4)
+z =x + y
+print(z)
