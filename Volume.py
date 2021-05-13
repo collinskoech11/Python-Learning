7ae4f210253636e0f6880d233eb2153600c8b2ef
Diana-nyamai <dnyamai.dn@gmail.com>
2021-05-13 16:44:46 +0300

    pdate

--- Volume.py
+++ Volume.py
@@ -4,5 +4,7 @@ radius = float(input('enter radius'))
 height = float(input('enter height'))
 
 def Vol():
-Volume = math.pi * radius * radius * height
-print(Volume)
+    Volume = math.pi * radius * radius * height
+    return Volume
+
+print(Vol())
