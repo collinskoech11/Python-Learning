bd0f14f9a74104019c6152e90917a43f5d57fcbe
Diana-nyamai <dnyamai.dn@gmail.com>
2021-05-13 12:55:16 +0300

    roll dice.py update

--- RollDice/Roll_Dice.py
+++ RollDice/Roll_Dice.py
@@ -2,10 +2,12 @@ import random
 
 def main():
     player1 = 0
+    player1wins = 0
     player2 = 0
+    player2wins = 0
     rounds = 1
 
-    while rounds != 4:
+    while rounds != 10:
          print("Round" + str(rounds))
          player1 = roll_dice()
          player2 = roll_dice()
@@ -17,10 +19,21 @@ def main():
              print('draw')
 
          elif player1 > player2:
+             player1wins = player1wins + 1
              print("player 1 wins!")
 
          else:
+             player2wins = player2wins + 1
              print("player 2 wins!")
+
+    if player1wins == player2wins:
+        print("draw")
+
+    elif player1wins > player2wins:
+            print("player 1 wins - number of wins: " + str(player1wins))
+    
+    else:
+        print("player 2 wins - number of wins: " + str(player2wins))
 
 def roll_dice():
     rollDice = random.randint(1, 6)
