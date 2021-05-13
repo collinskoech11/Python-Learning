4339de25d35876bd6efc9fa14e971f7e9ccdb5e5
Diana-nyamai <dnyamai.dn@gmail.com>
2021-05-13 12:16:25 +0300

    update

--- RollDice/Roll_Dice.py
+++ RollDice/Roll_Dice.py
@@ -1,4 +1,20 @@
 import random
 
+def main():
+    player1 = 0
+    player2 = 0
+    rounds = 1
+
+    while rounds != 4:
+         print("Round" + str(rounds))
+         player1 = roll_dice()
+         player2 = roll_dice()
+         print("player 1 roll: " + str(player1))
+         print("player 2 roll: " + str(player2))
+         rounds = rounds + 1
+
 def roll_dice():
-    rolldice = random.randint(1, 6)
+    rollDice = random.randint(1, 6)
+    return rollDice
+
+main()
