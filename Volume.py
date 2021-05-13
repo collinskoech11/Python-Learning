97cfba2bdb70a9447b7c974faac473770a194f5d
Diana-nyamai <dnyamai.dn@gmail.com>
2021-05-13 16:42:40 +0300

    update

--- Volume.py
+++ Volume.py
@@ -1,6 +1,8 @@
 import math
-pi = 22/7
+
 radius = float(input('enter radius'))
 height = float(input('enter height'))
-Volume = pi*radius*radius * height
+
+def Vol():
+Volume = math.pi * radius * radius * height
 print(Volume)
