44d153977ca47460e1c4137aa20a998f26e1c595
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-05 18:28:14 +0300

    update

--- Reverse.py
+++ Reverse.py
@@ -1,8 +1,12 @@
 def reversNumber(num):
 
-    #converting to integer
+    #converting to string
     string = int(num)
 
     #reversing the string
     string = list(string)
     string.reverse()
+    string = ''.join(string)
+
+    #coverting to s
+    num = int(string)
