60ecd83bd4f61f62904d1f84de594b894a5defa4
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-05 17:20:49 +0300

    update

---
+++ Reverse.py
@@ -0,0 +1,11 @@
+def reverseNum(num):
+    #converting to string
+    string = str(num)
+
+    #reversing the string
+    string = list(string)
+    string.reverse()
+    string = ''.join(string)
+
+    #converting string to integer
+
