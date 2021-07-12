4da6721108b222dab58ec6eec3eb24a68b7b53aa
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-12 11:33:58 +0300

    update

--- Reverse3.py
+++ Reverse3.py
@@ -2,4 +2,8 @@ def Reverse(num):
     stringNumber = str(num)
     reverseString = stringNumber[::-1]
     reversedNumber = int(reverseString)
+    return reversedNumber
+    
+num = input('enter a number')
+print(Reverse)
 
