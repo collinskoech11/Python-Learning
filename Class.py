23595d5487512f523fcb6f06ba0da687631de704
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-17 12:55:13 +0300

    update

--- Class.py
+++ Class.py
@@ -1,11 +1,13 @@
-class Person:
-    def __init__(self, firstname, lastname):
-        self.firstname = firstname
-        self.lastname = lastname
+class table:
+    def __init__(self, name, last):
+        self.name = name
+        self.last = last
+    
+    def bark(self):
+        print('woof')
 
-fistname = input('enter first name: ')
-lastname = input('enter last name: ')
-p1 = Person(fistname, lastname)
-print('Your first name is '+ p1.firstname + ' and the last name is ' + p1.lastname)
-
-        
+final = table("diana","ndinda")
+print(final.last)
+final.bark()
+#class
+         
