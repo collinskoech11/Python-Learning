672c6b0192b217188234df97a50e48d14668ba22
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-17 13:58:17 +0300

    update

--- Authentication/authentication.py
+++ Authentication/authentication.py
@@ -1,2 +1,7 @@
  # this is asimple login authentcation program
 print('Create an acccount')
+username = input('Enter your name: ')
+password = input('Enter password: ')
+
+print('Created account successfully')
+print('Please login')
