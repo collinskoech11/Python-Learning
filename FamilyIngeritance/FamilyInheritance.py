8df4828cc8aaa266db3926c57d429fff797e1685
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-17 13:11:21 +0300

    update

--- FamilyIngeritance/FamilyInheritance.py
+++ FamilyIngeritance/FamilyInheritance.py
@@ -1,3 +1,11 @@
 class Family:
     def __init__(self, name, complexion):
+        self.name = name
+        self.complexion = complexion
+
+class GrandKid(Family):
+    def height(self):
+        print('she is taller')
+
+
        
