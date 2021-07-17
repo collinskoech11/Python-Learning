5f056f7917d08a1b2bc7cbd78d89acdf6d189923
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-17 13:13:14 +0300

    update

--- FamilyIngeritance/FamilyInheritance.py
+++ FamilyIngeritance/FamilyInheritance.py
@@ -7,5 +7,8 @@ class GrandKid(Family):
     def height(self):
         print('she is taller')
 
+name = input('enter your name: ')
+complexion = input('enter your complexion')
+g1 = GrandKid(name, complexion)
 
        
